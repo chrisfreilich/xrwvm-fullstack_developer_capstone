346c6b285d43b83c279897a43bfa5a1a2b67e640
Chris Freilich <chrisfreilich@gmail.com>
2024-06-21 15:04:28 -0400

    More linting.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 
-                 'https://chrisfreilic-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = [
-    'https://chrisfreilic-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+url = 'https://chrisfreilic-8000.theiadockernext-0-labs' + \
+    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+
+ALLOWED_HOSTS = ['localhost', url]
+CSRF_TRUSTED_ORIGINS = [url]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,7 +97,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation' + \
+            '.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
